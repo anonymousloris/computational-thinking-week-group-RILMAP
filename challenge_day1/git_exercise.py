457b9a6ca69d63064f1b438cd6e884470ce94d6f
anonymousloris <167306036+anonymousloris@users.noreply.github.com>
2024-09-03 10:00:43 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,1 +1,18 @@
+# Importing the name variables from each team member's file
+import Ilias
+import Lumi
+import Rafael
+import anna
+import Philine
+
+def team_intro():
+    print("This is Team RILMAP. We are:")
+    print(Ilias.name)
+    print(Lumi.name)
+    print(Rafael.name)
+    print(anna.name)
+    print(Philine.name)
+
+if __name__ == "__main__":
+    team_intro()
 
