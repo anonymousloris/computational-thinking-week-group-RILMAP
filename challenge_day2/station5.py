56b53d2b08157b4626377842639dd669f16f0e55
anonymousloris <167306036+anonymousloris@users.noreply.github.com>
2024-09-03 10:52:18 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,1 +1,12 @@
+def solution_station_1():
+    names = ["Kimiya", "Tadas", "Anouk"]
 
+    def custom_rule(name):
+        
+        unique_letters = set(name.lower())
+        return len(unique_letters)  
+
+    outputs = [custom_rule(name) for name in names]
+    return outputs
+
+print(solution_station_1())
